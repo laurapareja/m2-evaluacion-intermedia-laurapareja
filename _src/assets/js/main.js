a0979786ad3c495e1c119af1fa9c07aa3dc4cb02
Laura Pareja <lauraparejalmodovar@gmail.com>
2019-07-29 17:09:51 +0200

    trying

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,27 +1,31 @@
 'use strict';
 debugger
-const citaCoelho = new Array(10)
-citaCoelho[0] = "./assets/images/cita1.png";
-citaCoelho[1] = "./assets/images/cita2.jpg";
-citaCoelho[2] = "./assets/images/cita3.png";
-citaCoelho[3] = "./assets/images/cita4.png";
-citaCoelho[4] = "./assets/images/cita5.jpg";
-citaCoelho[5] = "./assets/images/cita6.png";
-citaCoelho[6] = "./assets/images/cita7.png";
-citaCoelho[7] = "./assets/images/cita8.jpg";
-citaCoelho[8] = "./assets/images/cita9.jpg";
-citaCoelho[9] = "./assets/images/cita10.jpg";
 
+function getRandomArbitrary(min, max) {
+    const randomNumber = parseInt(Math.random() * (max - min) + min);
+    return randomNumber;
+}
 
-function choosepicture(event) {
-    const numberResult = getRandomArbitrary(0, 10);
-    document.getElementById("myImg").src = citaCoelho[numberResult];
+function getMessage(event) {
+    const numberResult = getRandomArbitrary(0, 100);
+    const message = document.querySelector(".js-message");
+    const inputNumber = document.querySelector(".js-inputNumber");
+    const inputNumberContent = document.querySelector(".js-inputNumber").value;
+    debugger
 
-    function getRandomArbitrary(min, max) {
-        const randomNumber = parseInt(Math.random() * (max - min) + min);
-        return randomNumber;
+    if (inputNumber === numberResult) {
+        message.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    } else if (inputNumber < numberResult) {
+        message.innerHTML = 'demasiado bajo';
+    } else {
+        message.innerHTML = 'demasiado alto';
     }
-};
+    console.log(Math.ceil(numberResult));
+    console.log(numberResult);
+    console.log(inputNumber);
+    console.log(inputNumberContent);
+}
+
 
 const button = document.querySelector('.js-button');
-button.addEventListener('click', choosepicture);
+button.addEventListener('click', getMessage);
